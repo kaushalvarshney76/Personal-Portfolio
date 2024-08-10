2b4e06b5fec177a86fd912f3cda9a5ee0d843c8f
Kaushal Varshney <kaushalvarshney76@gmail.com>
2024-08-10 14:36:08 +0530

    commit

--- dashboard/src/store/slices/skillSlice.js
+++ dashboard/src/store/slices/skillSlice.js
@@ -114,12 +114,13 @@ export const addNewSkill = (data) => async (dispatch) => {
         headers: { "Content-Type": "multipart/form-data" },
       }
     );
+    console.log("add new skills is: ", response);
     console.log("add new skills is: ", response.data);
     // console.log(response.data.message);
     dispatch(skillSlice.actions.addNewSkillSuccess(response.data.message));
     dispatch(skillSlice.actions.clearAllErrors());
   } catch (error) {
-    dispatch(skillSlice.actions.addNewSkillFailed(error.response.data.message));
+    dispatch(skillSlice.actions.addNewSkillFailed(error.response.message));
   }
 };
 
